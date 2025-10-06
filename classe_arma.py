01874b26f4f2748a47cb0d00bbb12f19a842e30c
Fabio Monaco <fabiomonaco007@gmail.com>
2025-10-06 16:08:51 +0200

    Add files via upload

--- classe_arma.py
+++ classe_arma.py
@@ -3,6 +3,7 @@ import random
 class Arma:
     def __init__(self, nome:str, danno_minimo:str, danno_massimo:str, tipo:str):
         self.nome = nome
+        #Controllo dei limiti del danno (minimo e massimo)
         if danno_minimo >= 1:
             self.danno_minimo = danno_minimo
         else:
@@ -15,10 +16,12 @@ class Arma:
             print("Attenzione!! Il danno massimo è più piccolo della condizione.")
             self.danno_massimo = self.danno_minimo + 1
 
+        #Controllo di che tipo di arma si tratta
         if tipo == "mischia" or tipo == "distanza":
             self.tipo = tipo
         else:
             print("ERRORE! Il tipo dev'essere mischia o distanza")
+            self.tipo = "mischia" #Arma che viene inserita di default
 
     def get_danno(self) -> int:
         numero_casuale = random.randint(self.danno_minimo, self.danno_massimo)
@@ -26,4 +29,3 @@ class Arma:
     
     def __str__(self):
         return f"Nome: {self.nome}, ({self.danno_minimo}, {self.danno_massimo}), Tipo: {self.tipo}"
-            
